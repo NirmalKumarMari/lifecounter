60440e6cbe779dcabebbd64fc71927ff21048554
Nirmal Kumar Marimuthu <nirmalkumar.marimuthu0@gmail.com>
2026-01-29 15:07:45 -0800

    done

--- lifecounter/ContentView.swift
+++ lifecounter/ContentView.swift
@@ -20,11 +20,47 @@ struct ContentView: View {
                 playerView(name: "Player 2", life: $life2)
                 
             }
+            Spacer()
+            
+            if (life1 <= 0) {
+                Text("Player 1 LOSES!")
+                    .font(.title)
+                    .fontWeight(.bold)
+            }
+            else if (life2 <= 0) {
+                Text("Player 2 LOSES!")
+                    .font(.title)
+                    .fontWeight(.bold)
+            }
         }
         .padding()
     }
 }
 
+struct playerView: View {
+    let name: String
+    @Binding var life: Int
+    
+    var body: some View {
+        VStack(spacing: 20){
+            Text(name)
+                .font(.title2)
+            Text("\(life)")
+                .font(.largeTitle)
+            
+            
+            HStack{
+                Button("-5") {life -= 5}
+                Button("-") {life -= 1}
+                Button("+") {life += 1}
+                Button("+5") {life += 5}
+            }
+            .buttonStyle(.bordered)
+        }
+        .frame(maxWidth: .infinity)
+        
+    }
+}
 #Preview {
     ContentView()
 }
